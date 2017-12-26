b2003dcf1cfced1e73547c051030e6b666852921
zubayer-ahamed-m4 <zubayer.ahamed@metafour.com>
2017-12-26 23:44:51 +0600

    modified design

--- src/main/java/com/todo/services/DoneServices.java
+++ src/main/java/com/todo/services/DoneServices.java
@@ -1,5 +1,55 @@
 package com.todo.services;
 
-public class DoneServices {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+
+import com.todo.exception.TodoException;
+import com.todo.model.TodoItem;
+
+@Service
+public class DoneServices {
+	private List<TodoItem> todoList = new ArrayList<>();
+
+	public void addItem(TodoItem todoItem) throws TodoException {
+		if (todoList.stream().filter(r -> r.getId() == todoItem.getId()).count() > 0) {
+			throw new TodoException("Item Already Exist");
+		}
+		todoItem.setId(new Random().nextLong());
+		todoList.add(todoItem);
+	}
+
+	public void updateItem(TodoItem todoItem) throws TodoException {
+		List<TodoItem> list = todoList.stream().filter(r -> r.getId().equals(todoItem.getId()))
+				.collect(Collectors.toList());
+		if (list.isEmpty()) {
+			throw new TodoException("No items found for update");
+		}
+		todoList.remove(list.get(0));
+		todoList.add(todoItem);
+	}
+
+	public void deleteItem(TodoItem todoItem) throws TodoException {
+		List<TodoItem> list = todoList.stream().filter(r -> r.getId().equals(todoItem.getId()))
+				.collect(Collectors.toList());
+		if (list.isEmpty()) {
+			throw new TodoException("No items found for delete");
+		}
+		todoList.remove(list.get(0));
+	}
+
+	public TodoItem getById(long id) {
+		List<TodoItem> list = todoList.stream().filter(r -> r.getId().equals(id)).collect(Collectors.toList());
+		if (list.size() != 1) {
+			return null;
+		}
+		return list.get(0);
+	}
+
+	public List<TodoItem> getTodoList() {
+		return todoList;
+	}
 }
