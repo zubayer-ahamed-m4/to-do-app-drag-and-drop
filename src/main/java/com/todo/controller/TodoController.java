c3e58d4a59b9c4698d4a4da7cb0bdd0f53db49f9
zubayer-ahamed-m4 <zubayer.ahamed@metafour.com>
2017-12-29 14:11:36 +0600

    drag and drop final

--- src/main/java/com/todo/controller/TodoController.java
+++ src/main/java/com/todo/controller/TodoController.java
@@ -20,33 +20,26 @@ import com.todo.services.TodoServices;
 @Controller
 public class TodoController {
 
-	@Autowired
-	private TodoServices todoServices;
-	@Autowired
-	private InProgressServices inProgressServices;
-	@Autowired
-	private DoneServices doneServices;
+	@Autowired private TodoServices todoServices;
+	@Autowired private InProgressServices inProgressServices;
+	@Autowired private DoneServices doneServices;
 
 	@RequestMapping("/")
 	public String loadIndexPage(Model model) {
 		model.addAttribute("todoItem", new TodoItem());
 		model.addAttribute("todoItems", todoServices.getTodoList().size() == 0 ? null : todoServices.getTodoList());
-		model.addAttribute("inProgressItems",
-				inProgressServices.getTodoList().size() == 0 ? null : inProgressServices.getTodoList());
+		model.addAttribute("inProgressItems", inProgressServices.getTodoList().size() == 0 ? null : inProgressServices.getTodoList());
 		model.addAttribute("doneItems", doneServices.getTodoList().size() == 0 ? null : doneServices.getTodoList());
 		return "views/index";
 	}
 
 	@RequestMapping(value = "/addTodoItem", method = RequestMethod.POST)
-	public String addTodoItem(@ModelAttribute("todoItem") @Valid TodoItem todoItem, BindingResult result, Model model)
-			throws TodoException {
+	public String addTodoItem(@ModelAttribute("todoItem") @Valid TodoItem todoItem, BindingResult result, Model model) throws TodoException {
 		if (result.hasErrors()) {
 			model.addAttribute("todoItems", todoServices.getTodoList().size() == 0 ? null : todoServices.getTodoList());
 			return "views/index";
 		}
 		todoServices.addItem(todoItem);
-		// model.addAttribute("todoItems", todoServices.getTodoList().size() == 0? null
-		// : todoServices.getTodoList());
 		return "redirect:/";
 	}
 
@@ -54,14 +47,15 @@ public class TodoController {
 	public String updateTodoItem(@PathVariable("id") long id, Model model) throws TodoException {
 		model.addAttribute("todoItemById", todoServices.getById(id));
 		model.addAttribute("todoItems", todoServices.getTodoList().size() == 0 ? null : todoServices.getTodoList());
+		model.addAttribute("inProgressItems", inProgressServices.getTodoList().size() == 0 ? null : inProgressServices.getTodoList());
+		model.addAttribute("doneItems", doneServices.getTodoList().size() == 0 ? null : doneServices.getTodoList());
 		model.addAttribute("update", "true");
 		model.addAttribute("todoItem", new TodoItem());
 		return "views/index";
 	}
 
 	@RequestMapping(value = "/updateItem", method = RequestMethod.POST)
-	public String updateItem(@ModelAttribute("todoItemById") @Valid TodoItem todoItem, BindingResult result,
-			Model model) throws TodoException {
+	public String updateItem(@ModelAttribute("todoItemById") @Valid TodoItem todoItem, BindingResult result, Model model) throws TodoException {
 		if (result.hasErrors()) {
 			model.addAttribute("todoItems", todoServices.getTodoList().size() == 0 ? null : todoServices.getTodoList());
 			model.addAttribute("update", "true");
@@ -71,39 +65,4 @@ public class TodoController {
 		todoServices.updateItem(todoItem);
 		return "redirect:/";
 	}
-
-	@RequestMapping(value = "/moveToInProgress/{id}", method = RequestMethod.GET)
-	public String moveToInProgress(@PathVariable("id") long id, Model model) throws TodoException {
-		inProgressServices.addItem(todoServices.getById(id));
-		todoServices.deleteItem(todoServices.getById(id));
-		return "redirect:/";
-	}
-
-	@RequestMapping(value = "/moveToTodo/{id}", method = RequestMethod.GET)
-	public String moveToTodo(@PathVariable("id") long id, Model model) throws TodoException {
-		todoServices.addItem(inProgressServices.getById(id));
-		inProgressServices.deleteItem(inProgressServices.getById(id));
-		return "redirect:/";
-	}
-
-	@RequestMapping(value = "/moveToDone/{id}", method = RequestMethod.GET)
-	public String moveToDone(@PathVariable("id") long id, Model model) throws TodoException {
-		doneServices.addItem(inProgressServices.getById(id));
-		inProgressServices.deleteItem(inProgressServices.getById(id));
-		return "redirect:/";
-	}
-
-	@RequestMapping(value = "/moveToInProgressFromDone/{id}", method = RequestMethod.GET)
-	public String moveToInProgressFromDone(@PathVariable("id") long id, Model model) throws TodoException {
-		inProgressServices.addItem(doneServices.getById(id));
-		doneServices.deleteItem(doneServices.getById(id));
-		return "redirect:/";
-	}
-
-	@RequestMapping(value = "/deleteItem/{id}", method = RequestMethod.GET)
-	public String deleteItem(@PathVariable("id") long id, Model model) throws TodoException {
-		doneServices.deleteItem(doneServices.getById(id));
-		return "redirect:/";
-	}
-
 }
--- src/main/java/com/todo/controller/TodoRestController.java
+++ src/main/java/com/todo/controller/TodoRestController.java
@@ -5,20 +5,53 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.todo.exception.TodoException;
 import com.todo.message.Response;
+import com.todo.services.DoneServices;
+import com.todo.services.InProgressServices;
 import com.todo.services.TodoServices;
 
 @RestController
 @RequestMapping("/rest")
 public class TodoRestController {
 
-	@Autowired
-	private TodoServices todoServices;
+	@Autowired private TodoServices todoServices;
+	@Autowired private InProgressServices inProgressServices;
+	@Autowired private DoneServices doneServices;
 	
-	@RequestMapping(value="/updateTodoItem/{id}")
-	public Response getItemById(@PathVariable("id") long id){
-		Response response = new Response("Done", todoServices.getById(id));
-		return response;
+	@RequestMapping(value="/changeData/{fromId}/{itemId}/{toId}")
+	public String changeData(@PathVariable("fromId") String fromId, @PathVariable("itemId") long id, @PathVariable("toId") String toId) throws TodoException{
+		if(fromId.contains("todo") && toId.contains("inprogress")) {
+			inProgressServices.addItem(todoServices.getById(id));
+			todoServices.deleteItem(todoServices.getById(id));
+		}else if(fromId.contains("todo") && toId.contains("done")) {
+			doneServices.addItem(todoServices.getById(id));
+			todoServices.deleteItem(todoServices.getById(id));
+		}else if(fromId.contains("inprogress") && toId.contains("todo")) {
+			todoServices.addItem(inProgressServices.getById(id));
+			inProgressServices.deleteItem(inProgressServices.getById(id));
+		}else if(fromId.contains("inprogress") && toId.contains("done")) {
+			doneServices.addItem(inProgressServices.getById(id));
+			inProgressServices.deleteItem(inProgressServices.getById(id));
+		}else if(fromId.contains("done") && toId.contains("todo")) {
+			todoServices.addItem(doneServices.getById(id));
+			doneServices.deleteItem(doneServices.getById(id));
+		}else if(fromId.contains("done") && toId.contains("inprogress")) {
+			inProgressServices.addItem(doneServices.getById(id));
+			doneServices.deleteItem(doneServices.getById(id));
+		}
+		return "success";
 	}
 	
+	@RequestMapping(value="/deleteData/{parentId}/{id}")
+	public String deleteData(@PathVariable("parentId") String parentId, @PathVariable("id") long id) throws TodoException{
+		if(parentId.contains("todo")) {
+			todoServices.deleteItem(todoServices.getById(id));
+		}else if(parentId.contains("inprogress")) {
+			inProgressServices.deleteItem(inProgressServices.getById(id));
+		}else {
+			doneServices.deleteItem(doneServices.getById(id));
+		}
+		return "success";
+	}
 }
